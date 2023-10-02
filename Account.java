b6362aadcd512fb411d00e8d893fa9aad33da0f0
Jaclyn Foster <jaclynfoster89@gmail.com>
2023-10-02 14:15:07 -0500

    generated account number

--- Account.java
+++ Account.java
@@ -4,6 +4,7 @@ public abstract class Account implements BaseRate{
     String sSN;
     double balance;
 
+    static int index = 10000;
     String accountNumber;
     double rate;
 
@@ -13,6 +14,19 @@ public abstract class Account implements BaseRate{
         this.sSN = sSN;
         this.balance = initDeposit + 100;
         System.out.println("NAME: " + name + " SSN: " + sSN + " BALANCE: $" + balance);
+        
+        //Set Account Number
+        index++;
+        this.accountNumber = setAccountNumber();
+        System.out.println("ACCOUNT NUMBER" + this.accountNumber);
+
         }
+
+        private String setAccountNumber() {
+            String lastTwoOfSSN = sSN.substring(sSN.length()-2, sSN.length());
+            int uniqueID = index;
+            int randomNumber = (int) (Math.random() * Math.pow(10, 3));
+            return lastTwoOfSSN + uniqueID + randomNumber;
+        } 
     //List common methods
 }
