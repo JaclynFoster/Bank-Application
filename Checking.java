499c95fc2d63f3d6be0622c9716f7e1fc35a2cbd
Jaclyn Foster <jaclynfoster89@gmail.com>
2023-10-02 16:08:29 -0500

    implemented checking account debit card

--- Checking.java
+++ Checking.java
@@ -5,16 +5,28 @@ public class Checking extends Account {
     int debitCardPIN;
 
     //Constructor to initialize Checking Account properties
-        public Checking(String name, String sSN, double initDeposit) {
+    public Checking(String name, String sSN, double initDeposit) {
             super(name, sSN, initDeposit);
             accountNumber = "2" + accountNumber;
-            System.out.println("ACCOUNT NUMBER: " + this.accountNumber);
+            setDebitCard();
+            // System.out.println("ACCOUNT NUMBER: " + this.accountNumber);
             // System.out.println("NEW CHECKING ACCOUNT");
         }
     //List any methods specific to the Checking Account
+    private void setDebitCard() {
+        debitCardNumber = (int) (Math.random() * Math.pow(10,12));
+        debitCardPIN = (int) (Math.random() * Math.pow(10, 4));
+        System.out.println("CARD: " + this.debitCardNumber);
+        System.out.println("PIN: " + this.debitCardPIN);
+    }
+
     public void shownInfo() {
         super.showInfo();
-        System.out.println("ACCOUNT TYPE: Checking");
+        System.out.println("Your Checking Account features" + 
+        "\n Debit Card Number: " + debitCardNumber +
+        "\n Debit Card PIN: " + debitCardPIN
+        
+        );
     }
 
 }
